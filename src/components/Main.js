edd366919ff403b3a99574f7d82e712428dd4ebb
Tim Wenzel <twenzel@fdsrx.com>
2022-11-28 14:49:21 -0600

    data mocks addition

---
+++ src/components/Main.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Welcome from './Welcome.js';
+import HotelInfo from './HotelInfo';
+
+const Main = () => {
+return(
+    <main id="wrapper">
+        <Welcome />
+        <HotelInfo />
+    </main>
+);
+
+}
+
+export default Main;
